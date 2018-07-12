d00c863cd10ae0c8f0ef276b4f234caaf90c9182
jaskarans <jaskarans@selectsourceintl.com>
2018-07-12 09:32:04 +0530

    basic knowlege about angular

--- myProject/src/app/server/server.component.ts
+++ myProject/src/app/server/server.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+	allowNewServer = false;
+	serverId: number = 10;
+    serverStatus: string = "offine";
+    serverCreate = "Server Was not created!";
 
+   	serverName = '';
+    onCreateServer () {
+    	this.serverCreate = "Server was created :) ";
+    }
+
+    onUpdateServerName(event: Event){
+    	console.log(event);
+    	this.serverName = (<HTMLInputElement>event.target).value;
+    }
+
+    constructor() {
+    	setTimeout( 
+    		() => {
+    			this.allowNewServer = true;	
+    		}
+    	,2000);
+    }
+
+    ngOnInit(){
+
+    }
 }
